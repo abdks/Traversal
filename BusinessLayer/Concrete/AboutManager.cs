f40fb5b6ed963309a77beb998bafc8d9fc484d3f
AbdullahKus <62968246+abdks@users.noreply.github.com>
2024-02-09 16:23:43 +0300

    09/02/2024 --> 1.Commit

---
+++ BusinessLayer/Concrete/AboutManager.cs
@@ -0,0 +1,44 @@
+﻿using BusinessLayer.Abstract;
+using DatatAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+	public class AboutManager : IAboutService
+	{
+		IAboutDal _aboutDal;
+        public AboutManager(IAboutDal aboutDal)
+        {
+           _aboutDal = aboutDal;
+        }
+        public List<About> GetAll()
+		{
+			return _aboutDal.GetAll();
+		}
+
+		public About GetById(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void TAdd(About t)
+		{
+			_aboutDal.Insert(t);
+		}
+
+		public void TDelete(About t)
+		{
+			_aboutDal.Delete(t);
+		}
+
+		public void TUpdate(About t)
+		{
+			_aboutDal.Update(t);
+		}
+	}
+}
---
+++ DatatAccessLayer/EntityFreamework/EfAboutDal.cs
@@ -0,0 +1,16 @@
+﻿using DatatAccessLayer.Abstract;
+using DatatAccessLayer.Repository;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatatAccessLayer.EntityFreamework
+{
+	public class EfAboutDal :GenericRepository<About>,IAboutDal
+	{
+
+	}
+}
