4fd0bde8fffaa58ce1b39c9f95641395930d96d9
AbdullahKus <62968246+abdks@users.noreply.github.com>
2024-02-12 16:28:45 +0300

    12/02/2024 -->2.Commit

--- DatatAccessLayer/Migrations/20240212132820_mig123.Designer.cs
+++ DatatAccessLayer/Migrations/20240212132820_mig123.Designer.cs
@@ -3,6 +3,7 @@ using DatatAccessLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DatatAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20240212132820_mig123")]
+    partial class mig123
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Traversal/Controllers/DestinationController.cs
@@ -0,0 +1,28 @@
+﻿using BusinessLayer.Concrete;
+using DatatAccessLayer.EntityFreamework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Traversal.Controllers
+{
+    public class DestinationController : Controller
+    {
+        DestinationManager DestinationManager = new DestinationManager(new EfDestinationDal());
+
+        public IActionResult Index()
+        {
+            var values = DestinationManager.GetAll();
+            return View(values); return View();
+        }
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Details(Destination p)
+        {
+            return View();
+        }
+    }
+}
