844a71c29f934d0f91ec402ed6d0f7b2eb6c9351
AbdullahKus <62968246+abdks@users.noreply.github.com>
2024-02-13 14:40:29 +0300

    13/02/2024 --> 2.Commit

--- Traversal/Controllers/CommentController.cs
+++ Traversal/Controllers/CommentController.cs
@@ -8,19 +8,32 @@ namespace Traversal.Controllers
     public class CommentController : Controller
     {
         CommentManager commentManager = new CommentManager(new EfCommentDal());
+        //[HttpGet]
+        //public PartialViewResult AddComment()
+        //{
+        //    return PartialView();
+        //}
+        //[HttpPost]
+        //public PartialViewResult AddComment(Comment p)
+        //{
+        //    p.CommentDate = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+        //    p.CommentState = true;
+        //    commentManager.TAdd(p);
+
+        //    return PartialView();
+        //}
         [HttpGet]
         public PartialViewResult AddComment()
         {
             return PartialView();
         }
         [HttpPost]
-        public PartialViewResult AddComment(Comment p)
+        public IActionResult AddComment(Comment p)
         {
             p.CommentDate = Convert.ToDateTime(DateTime.Now.ToShortDateString());
             p.CommentState = true;
             commentManager.TAdd(p);
-    
-            return PartialView();
+            return RedirectToAction("Index", "Destination");
         }
     }
 }
