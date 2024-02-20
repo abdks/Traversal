d94715aa85bb6ddce14822a8d633d7a90cd0ec11
AbdullahKus <62968246+abdks@users.noreply.github.com>
2024-02-20 12:36:31 +0300

    20/02/2024 -->1.Commit

--- Traversal/Areas/Admin/Controllers/MailController.cs
+++ Traversal/Areas/Admin/Controllers/MailController.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DocumentFormat.OpenXml.Drawing;
+using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
 using MimeKit;
 using Traversal.Models;
 
 namespace Traversal.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class MailController : Controller
     {
         [HttpGet]
@@ -17,9 +20,19 @@ namespace Traversal.Areas.Admin.Controllers
         {
             MimeMessage mimeMessage = new MimeMessage();
 
-            MailboxAddress mailboxAddressFrom = new MailboxAddress(mailRequest.Name,mailRequest.SenderMail);
-            mimeMessage.From.Add(mailboxAddress);
-            mailboxAddress mailboxAddressTo = new MailboxAddress("User)
+            MailboxAddress mailboxAddressFrom = new MailboxAddress("Admin", "abdullahkus080@gmail.com");
+            mimeMessage.From.Add(mailboxAddressFrom);
+            MailboxAddress mailboxAddressTo = new MailboxAddress("User", mailRequest.ReceiverMail);
+            mimeMessage.To.Add(mailboxAddressTo);
+            var bodybuilder = new BodyBuilder();
+            bodybuilder.TextBody = mailRequest.Body;
+            mimeMessage.Body = bodybuilder.ToMessageBody();
+            mimeMessage.Subject = mailRequest.Subject;
+            SmtpClient smtpClient = new SmtpClient();
+            smtpClient.Connect("smtp.gmail.com", 587, false);
+            smtpClient.Authenticate("abdullahkus080@gmail.com", "orim taon nwtz wofs");
+            smtpClient.Send(mimeMessage);
+            smtpClient.Disconnect(true);
             return View();
         }
     }
