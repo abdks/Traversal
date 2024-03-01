5184f3dbc4b8cfb529dcfaae97f00f64b97c70d4
AbdullahKus <62968246+abdks@users.noreply.github.com>
2024-03-02 01:27:00 +0300

    02/03/2024 -->1.Commit

---
+++ SignalRApi/DAL/Visitor.cs
@@ -0,0 +1,19 @@
+﻿namespace SignalRApi.DAL
+{
+    public enum ECity
+    {
+        Edirne = 1,
+        İstanbul = 2,
+        Ankara = 3,
+        Bursa = 4,
+        Antalya = 5
+    }
+    public class Visitor
+    {
+        public int VisitorID { get; set; }
+        public ECity ECity { get; set; }
+        public int CityVisitCount { get; set; }
+        public DateTime VisitDate { get; set; }
+
+    }
+}
